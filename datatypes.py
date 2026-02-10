51dbfa506a288931185e78db1145e2758dadef12
gilajb <cjoybett@gmail.com>
2026-02-10 16:24:45 +0300

    Initial Commit

---
+++ datatypes.py
@@ -0,0 +1,47 @@
+#integer (whole numbers)
+age = 17
+
+#float (decimal values)
+temp = 32.5
+
+#string (texts)
+greeting = "Hello there"
+
+#boolean (true or false)
+isMarried = False
+
+print(greeting)
+print("I am", age, "years old")
+print("The body temperature of the patient is" , temp, "degrees Celsius")
+print("Are you married?", isMarried)
+
+
+
+#    DATA STRUCTURE ----->
+cars = ["Audi", "BMW", "Toyota", "Mercedes"] #list - ordered & changeable
+
+fruits = ("Apple", "Banana", "Cherry", "Grapes") #tuple - ordered & unchangeable
+
+countries = {"Italy", "France", "Spain", "Germany"} #set - unordered
+
+student = {
+    "firstname" : "Mariana Coothe",
+    "age" : 22,
+    "course" : "FullStack",
+    "gender" : "Male"
+}
+
+print(cars)
+print(fruits)
+print(countries)
+print(student["firstname"])
+
+#Typecasting
+
+
+
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,18 @@
+#Declaring used!!!
+
+#variable = value --->
+
+firstname = "Joy"
+school = "eMobilis"
+lastname = "Bett"
+num1 = 56
+num2 = 55
+language = "Python"
+
+print(firstname, lastname)
+print(school)
+print(num1, num2)
+print(num1 + num2)
+print(language)
+print(school, "is a good school")
+print(language,"is a web programming language")
