065958e3d9384d712559a5e938ef917a9a381643
gilajb <cjoybett@gmail.com>
2026-02-14 23:38:26 +0300

    Initial Commit

---
+++ evenoddnumbers.py
@@ -0,0 +1,7 @@
+x = int(input("Enter any number: "))
+
+if x % 22 == 0:
+    print(x, "is an even number")
+
+else:
+    print(x, "is an odd number")
---
+++ loops.py
@@ -0,0 +1,11 @@
+#While loop --> if repeated processing is required
+x = -9
+
+while x <= 5:
+    print(x)
+    x = x + 1  # or (x +=1)
+
+
+
+#
+
